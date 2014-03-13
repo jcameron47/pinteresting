8f16cc6acaf618b2c4c9473d360bcb45c3042b3d
Jonathan Cameron <jacprogrammer@hotmail.com>
2014-03-13 08:30:39 -0700

    pins permission update

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,6 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, only: [:new]
 
   def index
     @pins = Pin.all
